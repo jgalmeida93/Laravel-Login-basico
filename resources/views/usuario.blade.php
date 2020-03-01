24d8daeb878b10a972c5b804979dba6c1ce096f8
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-03-01 11:39:51 -0300

    :pencil: Adiciona comentários

--- resources/views/usuario.blade.php
+++ resources/views/usuario.blade.php
@@ -1,3 +1,4 @@
+<!-- auth executa só se o usuário estiver logado -->
 @auth
 <h4>Você está logado</h4>
 <p> {{ Auth::user()->name }}</p>
@@ -5,6 +6,7 @@
 <p> {{ Auth::user()->id }}</p>
 @endauth
 
+<!-- guest executa só se o usuário não estiver logado -->
 @guest
     <h2>Olá visitante! Você não está logado.</h2>
 @endguest
