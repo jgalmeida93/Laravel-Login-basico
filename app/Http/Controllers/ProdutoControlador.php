a67b62fa097db2f05cf71eb9e01c3167895d1515
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-03-01 11:10:21 -0300

    :lipstick: Adiciona Bootstrap

--- app/Http/Controllers/ProdutoControlador.php
+++ app/Http/Controllers/ProdutoControlador.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ProdutoControlador extends Controller
 {
-    //
+    public function index() {
+        echo '<h4>Lista de produtos</h4>';
+        echo '<ul>';
+        echo '<li>Macarrão</li>';
+        echo '<li>Feijão</li>';
+        echo '<li>Carne bovina</li>';
+        echo '<li>Arroz</li>';
+        echo '<li>Leite</li>';
+        echo '</ul>';
+    }
 }
